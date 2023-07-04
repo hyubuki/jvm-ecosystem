0cdcc395aebb45edd81b110cf327ccb82dcd0484
hyubuki <suhyuk.ts@gmail.com>
2023-07-04 12:42:46 +0900

    1. project 생성 2. gradle 캐싱

---
+++ src/test/kotlin/philo/jvmlabtools/JvmLabToolsApplicationTests.kt
@@ -0,0 +1,13 @@
+package hyubuki.jvmlabtools
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JvmLabToolsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
