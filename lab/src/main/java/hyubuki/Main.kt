32f20b723414f4e44553dcdb63e169a36aaad67b
hyubuki <suhyuk.ts@gmail.com>
2023-07-04 12:57:02 +0900

    1. (부분적) 멀티모듈 화 2. lab 모듈 추가

---
+++ lab/src/main/java/hyubuki/Main.kt
@@ -0,0 +1,13 @@
+package hyubuki
+
+import mu.KotlinLogging
+
+object Main {
+
+    private val log = KotlinLogging.logger { }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        log.info { "Hello world!" }
+    }
+}
