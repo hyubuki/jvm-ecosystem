bf11b202c309c54be13354c73e241d77fa057b2b
hyubuki <suhyuk.ts@gmail.com>
2023-07-06 11:59:00 +0900

    1. Simple SQL Generator 생성 2. FileManger 리펙터링 (IO, Object File 용도 분리)

--- sql-generator/src/main/java/philo/Main.java
+++
@@ -1,7 +0,0 @@
-package hyubuki;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ sql-generator/src/main/kotlin/hyubuki/SqlGenerator.kt
@@ -0,0 +1,11 @@
+package hyubuki
+
+import FileManager
+import ObjectManager
+
+class SqlGenerator
+
+fun main() {
+    val fileManager = FileManager()
+    fileManager.saveFile()
+}
