0e1b6756a63fe2272401804dfb121e092a3251e9
hyubuki <suhyuk.ts@gmail.com>
2023-12-12 21:09:09 +0900

    1. redis repo 생성 및 연동  2. kotlin all open lib 적용

---
+++ redis/src/main/kotlin/io/hyubuki/RedisApp.kt
@@ -0,0 +1,12 @@
+package io.hyubuki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedisApp {
+}
+
+fun main(args: Array<String>) {
+    runApplication<RedisApp>(*args)
+}
