4473b61cab4cd9e17e506c8cd43e594ba1bacf9b
hyubuki <suhyuk.ts@gmail.com>
2023-12-13 20:15:42 +0900

    ExtendedCrudRepository, Java버전 추가

---
+++ redis/src/main/java/io/hyubuki/JExtendedCrudRepository.java
@@ -0,0 +1,11 @@
+package io.hyubuki;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JExtendedCrudRepository<T, ID> extends CrudRepository<T, ID> {
+
+    default T findByIdOrNull(ID id) {
+
+        return findById(id).orElse(null);
+    }
+}
---
+++ redis/src/main/java/io/hyubuki/JLockRepository.java
@@ -0,0 +1,6 @@
+package io.hyubuki;
+
+import io.hyubuki.app.Lock;
+
+public interface JLockRepository extends JExtendedCrudRepository<Lock, String> {
+}
