86052dffb8af3278bbe9d36c0889d53246ea62c1
hyubuki <suhyuk.ts@gmail.com>
2023-12-29 09:59:29 +0900

    Reactive-X 수동 작성

---
+++ reactive-x/src/main/kotlin/io/philo/app/EventNotify.kt
@@ -0,0 +1,17 @@
+package io.hyubuki.app
+
+import org.springframework.stereotype.Component
+
+@Component
+class EventNotify(
+    val events: MutableList<String> = arrayListOf(),
+    var change: Boolean = false
+) {
+
+    fun add(data: String) {
+        events += data
+        change = true
+    }
+
+
+}
