10954d1abc401209cccbc0ea480387659683211e
hyubuki <suhyuk.ts@gmail.com>
2024-01-11 01:06:51 +0900

    [Docker 모듈] DB 연결용 repository 작성

---
+++ docker/src/main/kotlin/io/hyubuki/docker/app/Person.kt
@@ -0,0 +1,20 @@
+package io.hyubuki.docker.app
+
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+class Person {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+
+    @Column(nullable = false)
+    var name = ""
+
+    constructor() {
+        this.name = UUID.randomUUID().toString()
+    }
+}
