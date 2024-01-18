f18b1dc4f492aa62e3bc24860d68a41e8ef0b3a7
hyubuki <suhyuk.ts@gmail.com>
2024-01-18 18:20:27 +0900

    [Docker Module] add current package as bean dir

--- docker/src/main/kotlin/io/hyubuki/docker/DockerApp.kt
+++ docker/src/main/kotlin/io/hyubuki/docker/DockerApp.kt
@@ -3,7 +3,7 @@ package io.hyubuki.docker
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["io.hyubuki.app"])
+@SpringBootApplication(scanBasePackages = ["io.hyubuki.app", "io.hyubuki.docker"])
 class DockerApp
 
 fun main() {
