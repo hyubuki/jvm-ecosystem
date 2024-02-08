f38037a31a7df425c80253a1cd24363e43c177d2
hyubuki <suhyuk.ts@gmail.com>
2024-02-08 16:14:41 +0900

    RestTemplate KeepAlive 관련 설정

--- spring-event/src/main/kotlin/io/hyubuki/springevent/SpringEventApp.kt
+++ spring-event/src/main/kotlin/io/hyubuki/springevent/SpringEventApp.kt
@@ -3,9 +3,11 @@ package io.hyubuki.springevent
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 class SpringEventApp
 
 fun main() {
